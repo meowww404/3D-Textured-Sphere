1fbee69cba89268d7c1ebfa1f143451df6a6cb0d
jack <jackjack5810@yahoo.com>
2021-10-28 09:15:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Step-by-Step WebGPU Graphics Programming (35) 
+## Create a torus Wireframe
+
+This is the source code for the 35th part of a series YouTube videos on step-by-step WebGPU graphics programming.
+
+This sample WebGPU app creates a 3D textured sphere. The output from the app is shown in the following image.
+
+![image01](dist/assets/image01.png)
+
+## Link for All Source Code Used in the WebGPU Step-By-Step Video Series:
+
+https://github.com/jack1232/WebGPU-Step-By-Step
---
+++ src/main.ts
@@ -0,0 +1,51 @@
+import { LightInputs } from './shaders';
+import { CreateShapeWithTexture } from './texture';
+import { SphereData } from './vertex_data';
+import $ from 'jquery';
+
+const CreateShape = async (ul:number, vl:number, li:LightInputs = {}, textureFile:string, 
+    addressModeU:GPUAddressMode, addressModeV:GPUAddressMode, isAnimation = true) => {
+    const data = SphereData(2, 50, 30, [0,0,0], ul, vl);
+    await CreateShapeWithTexture(data?.vertexData!, data?.normalData!, data?.uvData!, textureFile, addressModeU, addressModeV, li, isAnimation);
+}
+
+let textureFile = 'earth.png';
+let addressModeU = 'repeat' as GPUAddressMode;
+let addressModeV = 'repeat' as GPUAddressMode;
+let li:LightInputs = {};
+let isAnimation = true;
+let ul = 1;
+let vl = 1;
+CreateShape(ul, vl, li, textureFile, addressModeU, addressModeV, isAnimation);
+
+$('#id-radio input:radio').on('click', function(){
+    let val = $('input[name="options"]:checked').val();
+    if(val === 'animation') isAnimation = true;
+    else isAnimation = false;
+    CreateShape(ul, vl, li, textureFile, addressModeU, addressModeV, isAnimation);
+});
+
+$('#btn-redraw').on('click', function(){
+    ul = parseFloat($('#id-ulength').val()?.toString() as string);
+    vl = parseFloat($('#id-vlength').val()?.toString() as string);
+    li.shininess = $('#id-shininess').val()?.toString() as string;  
+    CreateShape(ul, vl, li, textureFile, addressModeU, addressModeV, isAnimation);
+});
+
+$('#id-image').on('change',function(){
+    const ele = this as any;
+    textureFile = ele.options[ele.selectedIndex].value + '.png';
+    CreateShape(ul, vl, li, textureFile, addressModeU, addressModeV, isAnimation);
+});
+
+$('#id-addressu').on('change',function(){
+    const ele = this as any;
+    addressModeU = ele.options[ele.selectedIndex].value as GPUAddressMode;
+    CreateShape(ul, vl, li, textureFile, addressModeU, addressModeV, isAnimation);
+});
+
+$('#id-addressv').on('change',function(){
+    const ele = this as any;
+    addressModeV = ele.options[ele.selectedIndex].value;
+    CreateShape(ul, vl, li, textureFile, addressModeU, addressModeV, isAnimation);
+});
